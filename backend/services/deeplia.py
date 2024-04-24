972cb420a93e037a59fe711db382a2326f918a68
JuanEstebanR <juan1010.jerm@gmail.com>
2024-04-23 19:14:36 -0500

    Api traslation services

---
+++ backend/services/deeplia.py
@@ -0,0 +1,38 @@
+import os
+
+import deepl
+from dotenv import load_dotenv
+
+load_dotenv()
+DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
+
+
+class Deeplia:
+    """
+    Deeplia service class.
+    """
+
+    def __init__(self):
+        self.translator = deepl.Translator(DEEPL_API_KEY)
+
+    def translate_text(self, text, source_language, target_language):
+        """
+        Translate a string to a target language.
+        """
+        try:
+            response = self.translator.translate_text(text, source_lang=source_language, target_lang=target_language)
+        except Exception as e:
+            return str(e)
+        return response.text
+
+    def get_languages(self):
+        """
+        Get a list of supported languages.
+        """
+        try:
+            source_languages = [language.name for language in self.translator.get_source_languages()]
+            target_languages = [language.name for language in self.translator.get_target_languages()]
+        except Exception as e:
+            return str(e)
+
+        return {"source_languages": source_languages, "target_languages": target_languages}
